faeae1a65c83824f145c435d62aabccd890a54c1
aitemir <ajtemirulanbekuulu@gmail.com>
2022-04-16 23:37:43 +0600

    dom

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // document object model
-//
+
+const heading = document.getElementById('hello')
+// const heading2 = document.getElementsByTagName('h2')[0]
+// const heading2 = document.getElementsByClassName('h2-class')[0]
+const heading2 = document.querySelector('#sub-hello')
+
+// console.dir(heading.textContent)
+setTimeout(()=>{
+    heading.textContent = 'Changed from JavaScript!'
+},5000)
+
+function addStylesTo(node){
+
+}
+
+
+
+
